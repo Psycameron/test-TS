e38a2811dc626ac62ff0b783cef8b58ad68f39fc
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-07-13 21:10:08 +0300

    Create class

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 "use strict";
-var PaymentStatus;
-(function (PaymentStatus) {
-    PaymentStatus["Success"] = "success";
-    PaymentStatus["Failed"] = "failed";
-})(PaymentStatus || (PaymentStatus = {}));
-function isPaymentSuccess(res) {
-    if (res.status === PaymentStatus.Success) {
-        return true;
-    }
-    else {
-        return false;
+class User {
+    constructor(name) {
+        this.name = name;
     }
 }
-function getIdFromData(res) {
-    if (isPaymentSuccess(res)) {
-        return res.data.databaseId;
-    }
-    else {
-        throw new Error(res.data.errorMessage);
-    }
-}
-const successResult = {
-    status: PaymentStatus.Success,
-    data: {
-        databaseId: 567,
-        sum: 10000,
-        from: 2,
-        to: 4
-    }
-};
-getIdFromData(successResult);
+const user = new User("Dima");
+console.log(user);
+user.name = "Vika";
+console.log(user);
--- app.ts
+++ app.ts
@@ -1,63 +1,18 @@
-interface IPayment {
-	sum: number;
-	from: number;
-	to: number;
-}
-
-enum PaymentStatus {
-	Success = "success",
-	Failed = "failed",
-}
-
-interface IPaymentRequest extends IPayment { }
-
-interface IDataSuccess extends IPayment {
-	databaseId: number;
-}
-
-interface IDataFailed {
-	errorMessage: string;
-	errorCode: number;
-}
-
-interface IResponseSuccess {
-	status: PaymentStatus.Success;
-	data: IDataSuccess;
-}
-
-interface IResponseFailed {
-	status: PaymentStatus.Failed;
-	data: IDataFailed;
-}
-
-type foo = (res: IResponseSuccess | IResponseFailed) => number;
-
-type Res = IResponseSuccess | IResponseFailed;
-
-function isPaymentSuccess(res: Res): res is IResponseSuccess{
-    if (res.status === PaymentStatus.Success) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function getIdFromData(res: Res): number {
-	if (isPaymentSuccess(res)) {
-		return res.data.databaseId;
-	} else {
-		throw new Error(res.data.errorMessage)
+class User {
+	constructor(name: string) {
+		this.name = name;
 	}
+	name: string;
 }
 
-const successResult: IResponseSuccess = {
-    status: PaymentStatus.Success,
-	data: {
-		databaseId: 567,
-		sum: 10000,
-		from: 2,
-		to: 4
-	}
+const user = new User("Dima");
+console.log(user);
+user.name = "Vika";
+console.log(user);
+
+class Admin{
+	role: number;
 }
 
-getIdFromData(successResult);
+const admin = new Admin();
+admin.role = 1;
