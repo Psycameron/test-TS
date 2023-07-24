4f9b1f9e82e3cf4abce66f3238618ab46755eade
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-07-24 16:27:48 +0300

    This

--- app.js
+++ app.js
@@ -1,61 +1,28 @@
 "use strict";
-class Product {
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-    }
-}
-class Delivery {
-    constructor(date) {
-        this.date = date;
-    }
-}
-class DeliveryToHome extends Delivery {
-    constructor(date, address) {
-        super(date);
-        this.address = address;
-    }
-}
-class DeliveryToShop extends Delivery {
-    constructor(shopId) {
-        super(new Date());
-        this.shopId = shopId;
-    }
-}
-class Cart {
+class Payment {
     constructor() {
-        this.productList = [];
+        this.date = new Date();
+        this.getDateArrow = () => {
+            return this.date;
+        };
     }
-    addProduct(product) {
-        this.productList.push(product);
-    }
-    deleteProduct(productId) {
-        this.productList = this.productList.filter((product) => product.id !== productId);
-    }
-    countPrice() {
-        return this.productList.map((p) => p.price).reduce((p1, p2) => {
-            return p1 + p2;
-        }, 0);
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkOut() {
-        if (this.productList.length === 0) {
-            throw new Error("The cart is empty! Add the products");
-        }
-        if (!this.delivery) {
-            throw new Error("Choose delivery method");
-        }
-        return { success: true };
+    getDate() {
+        return this.date;
     }
 }
-const cart = new Cart();
-cart.addProduct(new Product(1, "Cookie", 30));
-cart.addProduct(new Product(2, "Cake", 100));
-cart.addProduct(new Product(3, "Chocolate", 70));
-cart.deleteProduct(1);
-cart.setDelivery(new DeliveryToHome(new Date(), "to home"));
-console.log(cart.countPrice());
-console.log(cart.checkOut());
+const p = new Payment();
+const user = {
+    id: 1,
+    paymentDate: p.getDate.bind(p),
+    paymentArrow: p.getDateArrow
+};
+console.log(p.getDate());
+console.log(user.paymentDate());
+console.log(user.paymentArrow());
+class PaymentPersistent extends Payment {
+    save() {
+        return super.getDate();
+        // return this.getDateArrow()
+    }
+}
+console.log(new PaymentPersistent().save());
--- app.ts
+++ app.ts
@@ -1,69 +1,32 @@
-class Product {
-	constructor(
-		public id: number,
-		public title: string,
-		public price: number,
-	) {	}
-}
+class Payment {
+	private date: Date = new Date() 
 
+	getDate(this: Payment) {
+		return this.date
+	}
 
-class Delivery{
-	constructor(public date: Date){}
-}
-
-class DeliveryToHome extends Delivery {
-	constructor(date: Date, public address: string) {
-		super(date);
+	getDateArrow = () => {
+		return this.date
 	}
 }
 
-class DeliveryToShop extends Delivery {
-	constructor(public shopId: number) {
-		super(new Date());
+const p = new Payment()
+
+const user = {
+	id: 1,
+	paymentDate: p.getDate.bind(p),
+	paymentArrow: p.getDateArrow
+}
+
+console.log(p.getDate())
+console.log(user.paymentDate())
+console.log(user.paymentArrow())
+
+class PaymentPersistent extends Payment {
+	save() {
+		return super.getDate()
+		// return this.getDateArrow()
 	}
 }
 
-type DeliveryOptions = DeliveryToHome | DeliveryToShop
-
-class Cart {
-	private productList: Product[] = [];
-	private delivery: DeliveryOptions;
-	
-	
-	public addProduct(product: Product): void {
-		this.productList.push(product);
-	}
-	
-	public deleteProduct(productId: number): void {
-		this.productList = this.productList.filter((product: Product) => product.id !== productId);
-	}
-
-	public countPrice(): number {
-		return this.productList.map((p: Product) => p.price).reduce((p1: number, p2: number) => {
-			return p1 + p2
-		}, 0)
-	}
-
-	public setDelivery(delivery: DeliveryOptions): void {
-		this.delivery = delivery;
-	}		
-	public checkOut() {
-		if (this.productList.length === 0) { 
-			throw new Error("The cart is empty! Add the products")
-		}
-		if (!this.delivery) {
-			throw new Error("Choose delivery method")
-		}
-
-		return { success: true };
-	}
-}
-	
-const cart = new Cart();
-cart.addProduct(new Product(1, "Cookie", 30))
-cart.addProduct(new Product(2, "Cake", 100))
-cart.addProduct(new Product(3, "Chocolate", 70))
-cart.deleteProduct(1);
-cart.setDelivery(new DeliveryToHome(new Date(), "to home"))
-console.log(cart.countPrice())
-console.log(cart.checkOut())
+console.log(new PaymentPersistent().save())
