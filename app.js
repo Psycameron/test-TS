de336a684d565796d5ba6795f6ddc81fd71901fd
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-07-27 20:44:55 +0300

    Generic

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 "use strict";
-// Необходимо реализовать абстрактный класс Logger с 2-мя методами
-// абстрактным - log(message): void
-// printDate - выводящий в log дату
-// К нему необходимо сделать реальный класс, который бы имел метод: logWithDate,
-// выводящий сначала дату, а потом заданное сообщение
-class Logger {
-    printDate(date) {
-        this.log(date.toString());
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const Arr = [1, 2, 3];
+const check = {
+    drive: true,
+    kpp: false
+};
+function test() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const a = yield new Promise((resolve, reject) => {
+            resolve(1);
+        });
+    });
 }
-class LoggerDate extends Logger {
-    log(message) {
-        console.log(message);
-    }
-    logWithDate(date, message) {
-        this.printDate(date);
-        this.log(message);
-    }
-}
-const l = new LoggerDate();
-l.logWithDate(new Date(), "hello");
--- app.ts
+++ app.ts
@@ -1,27 +1,12 @@
-// Необходимо реализовать абстрактный класс Logger с 2-мя методами
-// абстрактным - log(message): void
-// printDate - выводящий в log дату
-// К нему необходимо сделать реальный класс, который бы имел метод: logWithDate,
-// выводящий сначала дату, а потом заданное сообщение
+const Arr: Array<number> = [1, 2, 3]
 
-abstract class Logger {
-	abstract log(message: string): void;
-
-	printDate(date: Date) {
-		this.log(date.toString())
-	}
+const check: Record<string, boolean> = {
+	drive: true,
+	kpp: false
 }
 
-class LoggerDate extends Logger {
-	log(message: string): void {
-		console.log(message)
-	}
-
-	logWithDate(message: string) {
-		this.printDate(new Date())
-		this.log(message)
-	}
+async function test() {
+	const a = await new Promise<number>((resolve, reject) => {
+		resolve(1);
+	})
 }
-
-const l = new LoggerDate()
-l.logWithDate("hello")
